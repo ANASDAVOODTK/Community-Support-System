24f256342e61f579fddbdfc73ba845c8038d51a0
ANASDAVOODTK <anasdavoodtk1@gmail.com>
2022-04-27 01:26:30 +0530

    added a lot of things

--- app/src/main/java/com/live/clg_project/WasteBinActivity.java
+++ app/src/main/java/com/live/clg_project/WasteBinActivity.java
@@ -3,6 +3,8 @@ package com.live.clg_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,11 +15,12 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class WasteBinActivity extends AppCompatActivity {
     MqttHelper mqttHelper;
+    ImageView imgWasteBin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_waste_bin);
-
+        imgWasteBin = findViewById(R.id.imgWasteBin);
 
 
         try{
@@ -40,9 +43,34 @@ public class WasteBinActivity extends AppCompatActivity {
                         @Override
                         public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
 
-                            TextView textView = findViewById(R.id.textView3);
-                            textView.setText(mqttMessage.toString());
-                       }
+
+                            int a  = Integer.parseInt(mqttMessage.toString());
+                            double test= 100-(((double) a / 27)*100);
+                            String wasteStatus = String.format("%.2f", test);
+                            TextView textView = findViewById(R.id.txtWastePercentage);
+                            textView.setText(wasteStatus+" %");
+
+                            if(test<25)
+                            {
+                                imgWasteBin.setImageResource(R.drawable.ic_wastbin1);
+                            }
+
+                            if(test>25)
+                            {
+                                imgWasteBin.setImageResource(R.drawable.ic_wastbin2);
+                            }
+                            if(test>50)
+                            {
+                                imgWasteBin.setImageResource(R.drawable.ic_wastbin3);
+                            }
+
+                            if(test>75)
+                            {
+                                imgWasteBin.setImageResource(R.drawable.ic_wastbin4);
+                            }
+
+
+                        }
 
                         @Override
                         public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
